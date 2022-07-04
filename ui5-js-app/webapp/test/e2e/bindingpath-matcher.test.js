1bd49e34a850e923af7c9eb0b416bdf4fe571fce
Volker Buzek <volker.buzek@js-soft.com>
2022-07-04 22:58:17 +0200

    chore: minimize samples

--- ui5-js-app/webapp/test/e2e/bindingpath-matcher.test.js
+++
@@ -1,6 +0,0 @@
-describe("binding path matcher/locator", () => {
-    it("bindingPath: contextPath")
-    it("bindingPath: propertyPath")
-    it("bindingPath: context- and propertyPath")
-    it("bindingPath: regular expressions in declarative syntax")
-})
